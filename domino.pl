aec61bef1dadf5da99c4258d7bae293bea0a402d
vieno <scarletwine_vieno@yahoo.co.jp>
2011-12-27 16:50:36 +0900

    fix

--- domino.pl
+++ domino.pl
@@ -1,6 +1,5 @@
 use v6;
 
-
 grammar Lisp {
     token left { '(' };
     token right { ')' };
@@ -16,11 +15,9 @@ grammar Lisp {
     rule TOP { ^^ <sexpr> $$ }
 }
 
-class Evaluate {
+class Parse {
 
-    BEGIN { Any.^add_method("at_key", method (*@_) { return Nil }); }
-
-    method _spform () { return 'car' | 'cdr' | 'cons' | 'eq' | 'atom' | 'cond' | 'if' | 'define' | 'quote' }
+#    BEGIN { Any.^add_method("at_key", method (*@_) { return Nil }); }
 
     method left ($/) {
         make '(';
@@ -31,7 +28,7 @@ class Evaluate {
     }
 
     method num ($/) {
-        make ($/.Int + 1).Str;
+        make $/.Str;
     }
 
     method str ($/) {
@@ -59,11 +56,10 @@ class Evaluate {
     }
 
     method sexpr ($/) {
-        if $<sexpr>[0]<atom><spform> -> $_ {  };
         if $<atom> {
             make $<atom>.ast;
         } else {
-            make '(' ~ $<sexpr>.map(*.ast).join(" ") ~ ')';
+            make self!eval($<sexpr>);
         }
     }
 
@@ -71,8 +67,97 @@ class Evaluate {
         make $<sexpr>.ast;
     }
 
+    method !eval ($/) {
+        say "called";
+        return $/.Str;
+    }
 }
 
-my $str = '(cdr 1 2 (car nil 1) (hoge 0 "abc"))';
-my $m = Lisp.parse($str, actions => Evaluate);
-say $m.ast;
+class Evaluate is Parse {
+
+    has %!sym = {};
+
+    method left ($/) {
+        make '(';
+    }
+
+    method right ($/) {
+        make ')';
+    }
+
+    method num ($/) {
+        make $/.Str;
+    }
+
+    method str ($/) {
+        make $/.Str;
+    }
+
+    method literal ($/) {
+        make $<num>.?ast // $<str>.ast;
+    }
+
+    method symbol ($/) {
+        make $/.Str;
+    }
+
+    method spform ($/) {
+        make $/.Str;
+    }
+
+    method nil ($/) {
+        make 'nil';
+    }
+
+    method atom ($/) {
+        make $<nil>.?ast // $<spform>.?ast // $<literal>.?ast;
+    }
+
+    method sexpr ($/) {
+        if $<atom> {
+            make $<atom>.ast;
+        } else {
+            make self!eval($<sexpr>);
+        }
+    }
+
+    method TOP ($/) {
+        make $<sexpr>.ast;
+    }
+
+    method !eval ($/) {
+        say "override!";
+        if $/[0]<atom><spform> -> $_ { return self!speval($_.ast, $/[1..*]) }
+        elsif $/[0].ast ~~ %!sym { return self!funcval(%!sym{$/[0].ast}, $/[1..*]) }
+        else { return "@(" ~ $/.map(*.ast).join(" ") ~ ")"; }
+    }
+
+    method !speval ($spform, $/) {
+        given $spform {
+            when "quote" {
+                $/.elems == 1 or die "quote: wrong number of argument => " ~ $/.elems ~ "\n";
+                return $/[0].ast.substr(1);
+            }
+            when "cdr" {
+                my $m = Lisp.parse($/[0].ast, actions => Parse);
+                self!check_elem($m<sexpr><sexpr>);
+                return "(" ~ $m.<sexpr><sexpr>.map(*.ast)[1..*].join(" ") ~ ")";
+            }
+        }
+    }
+
+    method !funceval ($func, $/) {
+        return "@(" ~ $/.map(*.ast).join(" ") ~ ")";
+    }
+
+    method !check_elem ($/) {
+        none($/[0]>>.ast.match(/^\@\(/)).Bool or die "undefined funtion: " ~ $/[0]<sexpr>[0].ast ~ "\n";
+    }
+
+}
+
+
+my $str = '(cdr (cdr (quote (1 2 3 4))))';
+my $ev = Evaluate.new;
+my $m = Lisp.parse($str, actions => $ev);
+say "m = ",$m.ast;

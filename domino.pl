313f3bd88a78bcebee5a8507faccf70fe05e8293
vieno <scarletwine_vieno@yahoo.co.jp>
2011-12-30 20:23:31 +0900

    cons implemented

--- domino.pl
+++ domino.pl
@@ -7,7 +7,7 @@ grammar Lisp {
     token str { '"'\w+'"' }
     token literal { <num>||<str> }
     token symbol { \w+ }
-    token nil { 'nil' }
+    token nil { 'nil'|'(' <ws> ')' }
     token atom { <literal>||<nil>||<spform>||<symbol> }
     token spform { 'car' | 'cdr' | 'cons' | 'eq' | 'atom' | 'cond' | 'if' | 'define' | 'quote' }
     token dot { '.' }
@@ -127,18 +127,23 @@ class Evaluate is Parse {
     }
 
     method !eval ($/) {
-        say "override!";
+        # 特殊形式
         if $/[0]<atom><spform> -> $_ { return self!speval($_.ast, $/[1..*]) }
-        elsif $/[0].ast ~~ %!sym { return self!funcval(%!sym{$/[0].ast}, $/[1..*]) }
+        # シンボル名を解決できた場合
+        elsif $/[0].?ast ~~ %!sym { return self!funceval(%!sym{$/[0].ast}, $/[1..*]) }
+        # そうでない場合: 特殊形式の一部以外ではself!check_elemで死ぬようにする
         else { return "@(" ~ $/.map(*.ast).join(" ") ~ ")"; }
     }
 
     method !speval ($spform, $/) {
-        say $spform;
+        say "operation for: ", $spform;
         given $spform {
 
             when "quote" {
                 $/.elems == 1 or die "quote: wrong number of argument => " ~ $/.elems ~ "\n";
+                say $/[0]<atom>;
+                return $/[0].ast if $/[0]<atom>;
+                $/[0].ast.match(/^\@/) or die "error: something is wrong...: ", $/[0].ast;
                 return $/[0].ast.substr(1);
             }
 
@@ -158,11 +163,27 @@ class Evaluate is Parse {
 
             when "cons" {
                 $/.elems == 2 or die "cons: wrong number of argument =>" ~ $/.elems ~ "\n";
-                say $/;
-                my @m = $/.map({Lisp.parse($_.ast, actions => Parse)});
-                self!check_elem(any @m<sexpr><sexpr>);
 
-                say @m;
+                my @m;
+                # (cons '(list) '(list))の場合
+                if $/[0]<sexpr>[1]<sexpr> && $/[1]<sexpr>[1]<sexpr> {
+                    @m = $/.map({Lisp.parse($_.ast, actions => Parse)});
+                    self!check_elem(any @m<sexpr><sexpr>);
+                }
+                # (cons 'atom '(list))の場合
+                elsif $/[0]<sexpr>[1]<atom> && $/[1]<sexpr>[1]<sexpr> {
+                    @m = $/[0]<sexpr>[1]<atom>, Lisp.parse($/[1].ast, actions => Parse);
+                }
+                # (cons 'atom 'atom)の場合
+                elsif $/[0]<sexpr>[1]<atom> && $/[1]<sexpr>[1]<atom> {
+                    return self!reduce_ws("(" ~ $/[0]<sexpr>[1]<atom>.ast ~ " . " ~ $/[1]<sexpr>[1]<atom>.ast ~ ")");
+
+                }
+                # (cons '(list) 'atom)の場合
+                else {
+                    return self!reduce_ws("(" ~ $/[0].ast ~ " . "  ~ $/[1].ast ~ ")");
+                }
+                return self!reduce_ws("(" ~  @m.map(*.ast).join(" ") ~ ")");
             }
 
             when "atom" {
@@ -191,11 +212,14 @@ class Evaluate is Parse {
         none($/[0]>>.ast.match(/^\@\(/)).Bool or die "undefined funtion: " ~ $/[0]<sexpr>[0].ast ~ "\n";
     }
 
+    method !reduce_ws (Str $s is copy) {
+        return $s.subst(/\s+/, " ", :global);
+    }
 }
 
 
 #my $str = '(car (cdr (cdr (quote (1 2 3 4)))))';
-my $str = '(cons (quote (1 2)) (quote (3 4)))';
+my $str = '(cons 1 (quote 2))';
 my $ev = Evaluate.new;
 my $m = Lisp.parse($str, actions => $ev);
 say "m = ",$m.ast;
